32cd9200d6798f39a163bca9c6cc86614cddb144
pauldg <pauldegeest1@gmail.com>
2022-07-27 14:23:44 +0200

    added script for including new tools

--- process_yaml.py
+++ process_yaml.py
@@ -236,12 +236,31 @@ def update_revision_from_base(base_dict, updated_dict):
     return [merged_tool_list, new_tool_list, updated_tool_list]
 
 
+def merge_yamls(yaml1, yaml2):
+    """
+    Takes a yaml file with a list of tools and merges them with another.
+    Outputs a merged yaml
+    """
+    updated_tools_yaml = copy.deepcopy(yaml1)
+
+    # unique_tools = [dict(y) for y in set(tuple(x.items())
+    #                                      for x in yaml1['tools'])]
+
+    # copy base and updated tools entries (except the revisions list of each)
+    for tool_entry in yaml2['tools']:
+        if tool_entry not in yaml1['tools']:
+            updated_tools_yaml['tools'].append(tool_entry)
+
+    return updated_tools_yaml
+
 
 if __name__ == '__main__':
 
     argparser = argparse.ArgumentParser(description='Arguments to parse.')
     argparser.add_argument('--update', action='store_true', help='Update from a base file')
     argparser.add_argument('--merge', action='store_true', help='Only update revisions from a base file')
+    argparser.add_argument('--merge_yaml', action='store_true',
+                           help='merge 2 yamls')
     argparser.add_argument('--add_sections', action='store_true', help='Add/update sections labels when possible using a base (reference) file')
     argparser.add_argument('--lint', action='store_true', help='Lint the section labels/ids')
     argparser.add_argument('--tools_yaml', '-y', dest='tools_yaml', type=str, required=True,
@@ -267,6 +286,7 @@ if __name__ == '__main__':
         base_file = open(args.base_file_path)
         base_yaml = yaml.load(base_file)
         parsed_dict = update_from_base(base_yaml, tools_yaml)
+        
     elif args.merge:
         base_file = open(args.base_file_path)
         base_yaml = yaml.load(base_file)
@@ -285,6 +305,16 @@ if __name__ == '__main__':
             ret_dict['tools'] = tool_list
             with open(prefix+args.out_file, 'w') as outfile:
                 yaml.dump(ret_dict, outfile, default_flow_style=False)
+
+    elif args.merge_yaml:
+         
+        yaml1_path = open(args.base_file_path)
+        yaml1 = yaml.load(yaml1_path)
+        updated_yaml = merge_yamls(yaml1, tools_yaml)
+
+        with open(args.out_file, 'w') as outfile:
+            yaml.dump(updated_yaml, outfile,
+                      default_flow_style=False, explicit_start=True)
 
     else:
         if args.lint:
